fb42ae4350fe64bdbf922907947e54416771b34d
Michael Owings <mowings@turbosquid.com>
2015-01-06 14:06:17 -0600

    Get test params from envars

--- s3/s3_test.go
+++ s3/s3_test.go
@@ -2,30 +2,34 @@ package s3
 
 import (
 	"os"
+	"path"
 	"testing"
 )
 
 var key = os.Getenv("AWS_KEY")
 var secret = os.Getenv("AWS_SECRET")
+var download_uri = os.Getenv("DOWNLOAD_URI")
+var upload_uri = os.Getenv("UPLOAD_URI")
+var local_file = path.Base(download_uri)
 
 func TestConnect(t *testing.T) {
 	conn := New(key, secret, "us-east-1")
 	if !conn.IsConnected() {
-		t.Errorf("Could not connect to S3")
-	}
-}
-
-func TestUpload(t *testing.T) {
-	conn := New(key, secret, "us-east-1")
-	err := conn.UploadFile("test.txt", "s3://dev-test-mikey.hero3d.net/go-test/test.txt", "application/txt")
-	if err != nil {
-		t.Errorf("Unable to upload file: %s", err.Error())
+		t.Errorf("Could not connect to S3 -- we will need ListMyBuckets permission for this to work")
 	}
 }
 
 func TestDownload(t *testing.T) {
 	conn := New(key, secret, "us-east-1")
-	err := conn.DownloadFile("s3://dev-test-mikey.hero3d.net/go-test/test.txt", "test_downloaded.txt")
+	err := conn.DownloadFile(download_uri, local_file)
+	if err != nil {
+		t.Errorf("Unable to download file: %s (%s)", local_file, err.Error())
+	}
+}
+
+func TestUpload(t *testing.T) {
+	conn := New(key, secret, "us-east-1")
+	err := conn.UploadFile(local_file, upload_uri, "application/txt")
 	if err != nil {
 		t.Errorf("Unable to upload file: %s", err.Error())
 	}
